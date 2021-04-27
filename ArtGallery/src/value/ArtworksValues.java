160a6db52bdb902d1b5abbc5a22c5866281c47b0
kibalias <74294078+kibalias@users.noreply.github.com>
2021-04-28 02:36:41 +0800

    ArtworksValues Minor Modification

--- ArtGallery/src/value/ArtworksValues.java
+++ ArtGallery/src/value/ArtworksValues.java
@@ -11,59 +11,59 @@ public class ArtworksValues {
 					art_status;
 	private Float 	art_price;
 	
-	public String getArtCode() {
-		return art_code;
-	}
-	
 	public void setArtCode(String ArtCode) {
 		art_code = ArtCode;
 	}
 	
-	public String getArtTitle() {
-		return art_title;
+	public String getArtCode() {
+		return art_code;
 	}
 	
 	public void setArtTitle(String ArtTitle) {
 		art_title = ArtTitle;
 	}
 	
-	public String getArtStyle() {
-		return art_style;
+	public String getArtTitle() {
+		return art_title;
 	}
 	
 	public void setArtStyle(String ArtStyle) {
 		art_style = ArtStyle;
 	}
 	
-	public String getYearOfMaking() {
-		return year_of_making;
+	public String getArtStyle() {
+		return art_style;
 	}
 	
 	public void setYearOfMaking(String YearOfMaking) {
 		year_of_making = YearOfMaking;
 	}
 	
-	public String getArtist() {
-		return artist;
+	public String getYearOfMaking() {
+		return year_of_making;
 	}
 	
 	public void setArtist(String paramArtist) {
 		artist = paramArtist;
 	}
 	
-	public String getArtStatus() {
-		return art_status;
+	public String getArtist() {
+		return artist;
 	}
 	
 	public void setArtStatus(String ArtStatus) {
 		art_status = ArtStatus;
 	}
 	
-	public Float getArtPrice() {
-		return art_price;
+	public String getArtStatus() {
+		return art_status;
 	}
 	
 	public void setArtPrice(Float ArtPrice) {
 		art_price = ArtPrice;
+	}
+	
+	public Float getArtPrice() {
+		return art_price;
 	}
 }
